30f3214307fbe8f8d5c95022892f85d58cf89c0e
Rokas <rookas.rudzenskas@gmail.com>
2023-03-03 16:44:14 +0200

    feat: add onScraperComplete function
    This function will be triggered when the scraper is done.

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,10 @@ const functions = require("firebase-functions");
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.onScrappingComplete = functions.https.onRequest((request, response) => {
+exports.onScraperComplete = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+
+// https://19fb-88-135-27-201.ngrok.io/rookas-big-dataa/us-central1/onScraperComplete
