351fb9a7444b3ddbbfc81f46bfd5f7750e7701ea
Rokas <rookas.rudzenskas@gmail.com>
2023-03-03 16:54:52 +0200

    Add firebase admin
    This commit adds firebase admin to the project.

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,7 @@ const functions = require("firebase-functions");
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.onScraperComplete = functions.https.onRequest((request, response) => {
+  console.log('SCRAPE COMPLETE >>>>>> :', request.body);
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
